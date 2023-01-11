b90a921097fd34c2813ce76805d83550358fe61b
mingchiuli <mingchiuli@163.com>
2023-01-11 12:54:00 +0800

    20230111

--- src/main/java/com/chiu/megalith/websocket/handler/WSMessageHandler.java
+++ src/main/java/com/chiu/megalith/websocket/handler/WSMessageHandler.java
@@ -4,6 +4,7 @@ import com.chiu.megalith.common.utils.SpringUtils;
 import com.chiu.megalith.websocket.dto.MessageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
@@ -24,13 +25,13 @@ public class WSMessageHandler {
 
     @Bean("CoopMessageListener")
     //processMessage作为listener
-    MessageListenerAdapter wSMessageListener(WSMessageHandler wSMessageHandler) {
+    public MessageListenerAdapter wSMessageListener(WSMessageHandler wSMessageHandler) {
         return new MessageListenerAdapter(wSMessageHandler, "processMessage");
     }
 
     //在container内将queue和listener绑定
     @Bean("CoopMessageListenerContainer")
-    SimpleMessageListenerContainer wSMessageListenerContainer(ConnectionFactory connectionFactory,
+    public SimpleMessageListenerContainer wSMessageListenerContainer(ConnectionFactory connectionFactory,
                                                                      @Qualifier("CoopMessageListener") MessageListenerAdapter listenerAdapter,
                                                                      @Qualifier("COOP_QUEUE") Queue queue) {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
