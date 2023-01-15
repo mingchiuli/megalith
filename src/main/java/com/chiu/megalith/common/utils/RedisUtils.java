9b61fd8bd3220228a181751c2112465c11fc0aaf
mingchiuli <mingchiuli@163.com>
2023-01-15 20:16:15 +0800

    20230115

--- src/main/java/com/chiu/megalith/common/utils/RedisUtils.java
+++ src/main/java/com/chiu/megalith/common/utils/RedisUtils.java
@@ -20,25 +20,6 @@ public class RedisUtils {
 
     private final ObjectMapper objectMapper;
 
-    public boolean tryLock(String key, String val, long timeOut, long expireTime){
-        long start = System.currentTimeMillis();
-        for (;;) {
-            Boolean b = redisTemplate.opsForValue().setIfAbsent(key, val, expireTime, TimeUnit.MILLISECONDS);
-            if (Boolean.TRUE.equals(b)){
-                return true;
-            }
-            if (System.currentTimeMillis() - start > timeOut){
-                return false;
-            }
-        }
-    }
-
-    public void unLock(String key, String val){
-        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
-        RedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
-        redisTemplate.execute(redisScript, Collections.singletonList(key), val);
-    }
-
     public Long delete(String key, String val){
         String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
         RedisScript<Long> redisScript = new DefaultRedisScript<>(script, Long.class);
