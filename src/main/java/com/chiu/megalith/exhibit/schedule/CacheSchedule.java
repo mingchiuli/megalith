21972e0e4c5c0d1aeafc7ba4700533610b77b642
mingchiuli <mingchiuli@163.com>
2023-03-11 16:29:12 +0800

    20230311

--- src/main/java/com/chiu/megalith/exhibit/schedule/CacheSchedule.java
+++ src/main/java/com/chiu/megalith/exhibit/schedule/CacheSchedule.java
@@ -167,7 +167,7 @@ public class CacheSchedule {
 
                 }, executor);
 
-                CompletableFuture.allOf(var1, var2, var3, var4, var5, var6).get(5, TimeUnit.SECONDS);
+                CompletableFuture.allOf(var1, var2, var3, var4, var5, var6).get();
 
                 redisTemplate.opsForValue().set(
                         CACHE_FINISH_FLAG,
