ca79abbb641a8c1cde731a1936fe9c657c297d1b
mingchiuli <mingchiuli@163.com>
2023-03-14 15:38:07 +0800

    20230314

--- src/main/java/com/chiu/megalith/coop/dto/QuitDto.java
+++ src/main/java/com/chiu/megalith/coop/dto/QuitDto.java
@@ -8,4 +8,11 @@ import java.io.Serializable;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
-public class QuitDto extends MessageDto implements Serializable {}
+public class QuitDto extends MessageDto implements Serializable {
+    @EqualsAndHashCode(callSuper = true)
+    @Data
+    @SuperBuilder
+    @AllArgsConstructor
+    @SuppressWarnings("unused")
+    public static class Bind extends BaseBind implements Serializable {}
+}
