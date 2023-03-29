4bec71b42f1413abae59fafb2fbb56fceeebdc0f
mingchiuli <mingchiuli@163.com>
2023-03-30 04:32:11 +0800

    20230330

--- src/main/java/com/chiu/megalith/security/controller/TokenController.java
+++ src/main/java/com/chiu/megalith/security/controller/TokenController.java
@@ -1,19 +1,13 @@
 package com.chiu.megalith.security.controller;
 
-import com.chiu.megalith.base.jwt.JwtUtils;
 import com.chiu.megalith.base.lang.Result;
 import com.chiu.megalith.security.service.TokenService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.Map;
 
 /**
