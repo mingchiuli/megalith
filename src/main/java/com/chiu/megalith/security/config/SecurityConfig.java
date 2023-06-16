18ababfe8f31360e973c359531db9b549f7ea0f5
mingchiuli <mingchiuli@163.com>
2023-06-16 20:26:13 +0800

    20230616

--- src/main/java/com/chiu/megalith/security/config/SecurityConfig.java
+++ src/main/java/com/chiu/megalith/security/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +15,6 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 
 @Configuration(proxyBeanMethods = false)
-@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final LoginFailureHandler loginFailureHandler;
