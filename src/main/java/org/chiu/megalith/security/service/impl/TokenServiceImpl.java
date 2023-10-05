09ead5eff7d92a0e8b7ad1b5fe14671c86a47623
mingchiuli <mingchiuli@163.com>
2023-10-06 02:01:55 +0800

    20231006

--- src/main/java/org/chiu/megalith/security/service/impl/TokenServiceImpl.java
+++ src/main/java/org/chiu/megalith/security/service/impl/TokenServiceImpl.java
@@ -8,9 +8,7 @@ import org.chiu.megalith.security.vo.UserInfoVo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
