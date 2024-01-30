7c03b89cdb705f81530bb66f0bce070cdbf94acf
mingchiuli <mingchiuli@163.com>
2024-01-30 22:29:51 +0800

    替换为http,ws有长度限制

--- src/main/java/org/chiu/megalith/blog/controller/BlogMessageController.java
+++ src/main/java/org/chiu/megalith/blog/controller/BlogMessageController.java
@@ -1,7 +1,6 @@
 package org.chiu.megalith.blog.controller;
 
 import org.chiu.megalith.blog.req.BlogEditPushActionReq;
-import org.chiu.megalith.blog.req.BlogEditPushAllReq;
 import org.chiu.megalith.blog.service.BlogMessageService;
 import org.chiu.megalith.infra.utils.SecurityUtils;
 import org.springframework.messaging.handler.annotation.MessageMapping;
--- src/main/java/org/chiu/megalith/blog/service/BlogMessageService.java
+++ src/main/java/org/chiu/megalith/blog/service/BlogMessageService.java
@@ -1,7 +1,6 @@
 package org.chiu.megalith.blog.service;
 
 import org.chiu.megalith.blog.req.BlogEditPushActionReq;
-import org.chiu.megalith.blog.req.BlogEditPushAllReq;
 
 public interface BlogMessageService {
 
