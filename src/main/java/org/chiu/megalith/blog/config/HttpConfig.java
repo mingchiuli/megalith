df57f7d94e0fc010d7f521e10ef9fbbccaece049
limingjiu <limingjiu@kanzhun.com>
2024-02-28 12:43:37 +0800

    更换为restClient

--- src/main/java/org/chiu/megalith/blog/config/HttpConfig.java
+++ src/main/java/org/chiu/megalith/blog/config/HttpConfig.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 
 @Configuration(proxyBeanMethods = false)
@@ -25,13 +28,16 @@ public class HttpConfig {
     @Bean
     OssHttpService ossHttpService() {
 
+        JdkClientHttpRequestFactory requestFactory = new JdkClientHttpRequestFactory();
+        requestFactory.setReadTimeout(Duration.ofSeconds(10));
+
         RestClient client = RestClient.builder()
                 .baseUrl(baseUrl)
+                .requestFactory(requestFactory)
                 .defaultHeader(HttpHeaders.HOST, bucketName + "." + ep)
                 .build();
 
         RestClientAdapter restClientAdapter = RestClientAdapter.create(client);
-
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(restClientAdapter)
                 .build();
         return factory.createClient(OssHttpService.class);
